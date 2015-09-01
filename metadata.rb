9b1f39457e9f226dee6b8e7907e00f60d20a8ccf
Tim Smith <tsmith84@gmail.com>
2015-09-01 09:41:02 -0700

    Fix urls and add Debian to metadata

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,9 @@ license          'Apache 2.0'
 description      'Installs and Configures the Snort IDS ruleset updater Pulled Pork'
 version          '1.1.1'
 
+supports 'debian', '>= 7'
 supports 'ubuntu', '>= 10.04'
 depends 'ark'
 
-source_url 'https://github.com/tas50/pulledpork' if respond_to?(:source_url)
-issues_url 'https://github.com/tas50/pulledpork/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/tas50/chef-pulledpork' if respond_to?(:source_url)
+issues_url 'https://github.com/tas50/chef-pulledpork/issues' if respond_to?(:issues_url)
