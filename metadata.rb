5c45af195bae6a2efdaf287cc7799e4cdc603e6e
Tim Smith <tsmith84@gmail.com>
2015-08-31 23:20:20 -0700

    Update my e-mail and add new metadata

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,12 @@
 name             'pulledpork'
 maintainer       'Tim Smith'
-maintainer_email 'tim@cozy.co'
+maintainer_email 'tsmith84@gmail.com'
 license          'Apache 2.0'
 description      'Installs and Configures the Snort IDS ruleset updater Pulled Pork'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.1'
 
 supports 'ubuntu', '>= 10.04'
 depends 'ark'
+
+source_url 'https://github.com/tas50/pulledpork' if respond_to?(:source_url)
+issues_url 'https://github.com/tas50/pulledpork/issues' if respond_to?(:issues_url)
