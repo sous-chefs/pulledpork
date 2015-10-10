55b82bd9babf8608dd1760c28a45ae2f67f183ce
Tim Smith <tsmith84@gmail.com>
2015-10-09 21:05:38 -0700

    Add long_description metadata

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer       'Tim Smith'
 maintainer_email 'tsmith84@gmail.com'
 license          'Apache 2.0'
 description      'Installs and Configures the Snort IDS ruleset updater Pulled Pork'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.1'
 
 supports 'debian', '>= 7'
