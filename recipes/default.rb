103d34b55beb75d3e00f30bb560ee22179e12e9d
Tim Smith <tsmith@chef.io>
2018-02-19 21:11:01 -0800

    Update copyright header
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: pulledpork
 # Recipe:: default
 #
-# Copyright (c) 2015, Tim Smith
+# Copyright:: 2015-2018, Tim Smith
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
