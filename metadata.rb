0cb6bf347762b35183d8a116ba3436ade6de8a8d
Tim Smith <tsmith@chef.io>
2018-02-19 21:04:12 -0800

    Transfer ownership to Sous Chefs
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name             'pulledpork'
-maintainer       'Tim Smith'
-maintainer_email 'tsmith84@gmail.com'
-license          'Apache 2.0'
-description      'Installs and Configures the Snort IDS ruleset updater Pulled Pork'
+name 'pulledpork'
+maintainer 'Sous Chefs'
+maintainer_email 'help@sous-chefs.org'
+license 'Apache-2.0'
+description 'Installs and Configures the Snort IDS ruleset updater Pulled Pork'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.0'
+version '2.0.0'
 
 supports 'debian', '>= 7.0'
 supports 'ubuntu', '>= 12.04'
@@ -16,5 +16,6 @@ supports 'oracle'
 
 depends 'ark'
 
-source_url 'https://github.com/tas50/chef-pulledpork' if respond_to?(:source_url)
-issues_url 'https://github.com/tas50/chef-pulledpork/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/sous-chefs/pulledpork' if respond_to?(:source_url)
+issues_url 'https://github.com/sous-chefs/pulledpork/issues' if respond_to?(:issues_url)
+chef_version '>= 12.0' if respond_to?(:chef_version)
